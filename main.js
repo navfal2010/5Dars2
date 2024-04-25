1a365499029da3bc08e6bac2836fc8e0b88af813
navfal2010 <navfal808@gmail.com>
2024-04-25 10:41:17 +0500

    done

---
+++ main.js
@@ -0,0 +1,10 @@
+let first = prompt('Ismingizni kiriting')
+let second = +prompt('Space idingizni kiriting')
+let third = +prompt('Space passwordingizni kiriting')
+let all ={
+   name:first,
+   id:second,
+   password:third
+}
+
+console.log(all);
